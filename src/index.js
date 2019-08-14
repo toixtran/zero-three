2dd545938a774951258c324a9c6b653ddf01ba34
toi.xtran <d.noda0904@gmail.com>
2019-08-14 15:31:05 +0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { Projector } from "three/examples/jsm/renderers/Projector";
+import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
+import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
 
 // declare variables
 var scene, camera, renderer, loader, model, container, controls;
+var view_size = {width: window.innerWidth, height: window.innerHeight};
 
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 
-function loadHouseModel(model_path) {
+function loadHouseModel(model_path){
     let model_file_extension = model_path.split('.').pop();
     switch (model_file_extension) {
         case 'gltf':
-            loadGLTFModel(model_path);
+            loader = new GLTFLoader();
+            break;
+        case 'dae':
+            loader = new ColladaLoader();
             break;
         default:
             break;
     }
+    loadModel(model_path, loader);
 }
 
-function loadGLTFModel(model_path) {
-    // Load gltf model
-    loader = new GLTFLoader();
+function loadModel(model_path, loader){
     loader.load(
         model_path,
-        gltf => {
-            model = gltf.scene;
+        model_type => {
+            model = model_type.scene;
 
             // setup center values
             let bbox = new THREE.Box3().setFromObject(model);
@@ -40,22 +44,19 @@ function loadGLTFModel(model_path) {
             bbox.getCenter(cent);
             bbox.getSize(size);
 
-            // center model
-            model.position.x -= cent.x;
-            model.position.y -= cent.y;
-            model.position.z -= cent.z;
-
             // add model to scene
+            model.updateMatrix();
+            model.position.set(0, 0, 0);
             scene.add(model);
         },
         xhr => {
             // handle load progress here
         },
-        error => console.log("glTF.load() error", error)
+        error => console.log("loadModel() error", error)
     );
 }
 
-function addLight(scene) {
+function addLight(scene){
     var lights = [];
     lights[ 0 ] = new THREE.PointLight( 0xffffff, .4, 0 );
     lights[ 0 ].position.set( 0, 200, 0 );
@@ -68,78 +69,65 @@ function addLight(scene) {
     scene.add( light );
 }
 
-function onDocumentMouseDown(event) {
-    // the following line would stop any other event handler from firing
-    // (such as the mouse's TrackballControls)
-    // event.preventDefault();
-
-    // update the mouse variable
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-    raycaster.setFromCamera( mouse, camera );
-    var intersects = raycaster.intersectObjects( scene.children );
-    if(intersects.length){
-        console.log(intersects[0]);
-        camera.position.set(intersects[0].point.x, 0.1, intersects[0].point.z)
-        console.log("camera's potions");
-        console.log(camera.position);
-        camera.updateProjectionMatrix();
-    }
-}
 
 function addCube(x, y,z){
-    var geometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
     var cube = new THREE.Mesh( geometry, material );
     cube.position.set(x, y, z);
     scene.add( cube );
 }
 
+function mouseMove(event){
+    let scale = 1;
+    mouse.x = - ( event.clientX / renderer.domElement.clientWidth ) * 2 + 1;
+    mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+    camera.rotation.x = mouse.y / scale;
+    camera.rotation.y = mouse.x / scale;
+}
+
 function init(){
     // get container
     container = document.getElementById("3d-decor");
+    view_size.width = window.getComputedStyle(container).width.slice(0, -2);
+    view_size.height = window.getComputedStyle(container).height.slice(0, -2);
 
     // setup basic scene/camera/renderer camera
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xd6e6ff);
-    camera = new THREE.PerspectiveCamera(
-        50,
-        container.clientWidth / container.clientHeight,
-        0.1,
-        1000
-    );
-
-    renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        alpha: true
-    });
+    camera = new THREE.PerspectiveCamera(50, view_size.width / view_size.height, 0.01, 100);
+    renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
     renderer.setPixelRatio(devicePixelRatio);
     renderer.setSize(container.clientWidth, container.clientHeight);
     // recommended configs by Three.js when loading glTF model
     renderer.gammaOutput = true;
     renderer.gammaFactor = 2.2;
+
     var houseModel = "/apartment/scene.gltf";
+    // var houseModel = "/apartment2/apartment.dae";
+    // var houseModel = "/apartment_floorplan/scene.gltf";
     loadHouseModel(houseModel);
     addLight(scene);
 
-    // addCube(0,0,0);
-    addCube(0,0,-0.4);
-
     // render
     container.appendChild(renderer.domElement);
-    controls = new OrbitControls( camera, renderer.domElement );
-    camera.position.set(0, 0.1, 0);
+    // controls = new OrbitControls( camera, renderer.domElement );
+    controls = new PointerLockControls(camera);
+    controls.enabled = true;
+    scene.add(controls.getObject());
+    camera.position.set(0, 0.2, -0.2);
+
+    camera.rotation.order = "YXZ"; // this is not the default
+    document.addEventListener( "mousemove", mouseMove, false );
 
     // when the mouse moves, call the given function
-    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+    // document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 
-    console.log(camera.position);
+    // Execute Render
     renderer.setAnimationLoop(() => {
         if (model) {
             // model.rotation.y += 0.02;
         }
-        controls.update();
         renderer.render(scene, camera);
     });
 }
@@ -147,3 +135,25 @@ function init(){
 init();
 
 
+
+
+// function onDocumentMouseDown(event) {
+//     // the following line would stop any other event handler from firing
+//     // (such as the mouse's TrackballControls)
+//     // event.preventDefault();
+
+//     // update the mouse variable
+//     mouse.x = ( event.clientX / view_size.width ) * 2 - 1;
+//     mouse.y = - ( event.clientY / view_size.height ) * 2 + 1;
+
+//     raycaster.setFromCamera( mouse, camera );
+//     console.log(scene.children);
+//     var intersects = raycaster.intersectObjects( scene.children );
+//     if(intersects.length){
+//         console.log(intersects);
+//         // camera.position.set(intersects[0].point.x, 0.1, intersects[0].point.z)
+//         // console.log("camera's potions");
+//         // console.log(camera.position);
+//         // camera.updateProjectionMatrix();
+//     }
+// }
